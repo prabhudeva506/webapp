22ee4611187c352be33b3d7af4c33c7566fbb95f
prabhu <prabhumunna412@gmail.com>
2018-03-21 12:44:21 -0500

    fourth

--- main.js
+++ main.js
@@ -14,5 +14,35 @@ xhr.send();
 jsondata("data.json",function(text) {
 let data= JSON.parse(text);
 console.log(data);
-
+ba(data.basics);
 })
+//main div class calling from html
+// var main=document.querySelector('main');
+var main=document.getElementById('main');
+//left div creation
+var left=document.createElement("div");
+left.classList.add("left");
+//id for left div
+left.setAttribute("id","left");
+//appending main div
+main.appendChild(left);
+console.log(left);
+function ba(basic){
+  //creation of img
+var img=document.createElement("img");
+img.src=basic.photo;
+left.appendChild(img);
+//name creation
+var name=document.createElement("h1");
+name.textContent=basic.name;
+left.appendChild(name);
+//creating email
+var mail=document.createElement("p");
+mail.textContent=basic.email;
+name.appendChild(mail);
+//mobile appending
+var mb=document.createElement("h4");
+mb.textContent=basic.mobile;
+mail.appendChild(mb);
+
+}
